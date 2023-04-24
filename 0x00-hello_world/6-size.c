b46f0316e4483a8883b13657285718bb881b0a96
chriselecandy <chriselecandy@gmail.com>
2023-04-24 09:06:33 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - C program that prints the size of various types on the computer it is compiled and run on
+ * main - print out sizes of data types in c
  *
  * Return: 0
 */
 int main(void)
-{       
-        int a;
-        long int b;
-        long long int c;
-        char d;
-        float f;
-        
-        printf("Size of d char: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("Size of b long int: %lu byte(S)\n", (unsigned long)sizeof(b));
-	printf("Size of c long long int: %lu byte(S)\n", (unsigned long)sizeof(c));
-	printf("Size of f float: %lu byte(S)\n", (unsigned long)sizeof(f));
-	return (0);
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("Size of d char: %lu byte(S)\n", (unsigned long)sizeof(d));
+printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(a));
+printf("Size of b long int: %lu byte(S)\n", (unsigned long)sizeof(b));
+printf("Size of c long long int: %lu byte(S)\n", (unsigned long)sizeof(c));
+printf("Size of f float: %lu byte(S)\n", (unsigned long)sizeof(f));
+return (0);
 }
