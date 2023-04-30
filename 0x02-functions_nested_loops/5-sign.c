2b242a0bac553312b112ce98c3fb2ae422feb768
chriselecandy <chriselecandy@gmail.com>
2023-04-30 08:19:14 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,27 @@
 #include "main.h"
+
 /**
- * print_sign - Determines if the input number
- * greater,equal or less than zero.
- *
- * @n: The input number as an integer.
- *
- * Return: 1 is greater than zero.0 is zero.
- * -1 is less than zero
+ * print_sign - Prints the sign of a number
+ * @n: The input number to be checked
+ * Return: 1 for positive um, -1 for negative num or zero for anything else
  */
+
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar(48);
-		return(0);
+		return (0);
 	}
-	_putchar('n\');
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * print_last_digit - Prints the last digit
- * of a number.
- *
- * @n:input number as an integer.
- *
- * Return: last digit.
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number to be treated
+ * Return: Value of the last digit of a number
  */
-int print_last_digit(int n)
-{
-	int l;
 
-	l = n % 10;
-	if (l < 0)
+int print_last_digit(int n)
+
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
 	{
-		_putchar(-l + 48);
-		return (-l);
+		last = last * -1;
 	}
-	else
-	{
-		_putchar(l + 48);
-		return (l);
-	}
+	_putchar(last + '0');
+	return (last);
 }
